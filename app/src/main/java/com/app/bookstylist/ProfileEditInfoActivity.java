c61f794e050be464b1426e0fbc42f2db307a16a8
Duong Kha Toan <toanduong.31191026877@st.ueh.edu.vn>
2022-04-22 22:07:10 +0700

    Push Done Profile favorite

---
+++ app/src/main/java/com/app/bookstylist/ProfileEditInfoActivity.java
@@ -0,0 +1,14 @@
+package com.app.bookstylist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfileEditInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_edit_info);
+    }
+}
